48a9ca7714a2683da7e6b40e286e3610501d316a
Lellansin <lellansin@gmail.com>
2014-11-09 14:14:07 +0800

    update http test

--- test/http.js
+++ test/http.js
@@ -12,15 +12,15 @@ url = 'http://lellansin.com/test/timeout.php';
 
 var deal = function(res) {
     if (res.statusCode == 200) {
+        // normal status
         res.on('data', function(chunk) {
             var content = chunk.toString();
             console.log(content.toString());
         });
     } else {
+        // exception status
         console.error('http status', res.statusCode);
     }
 };
 
-http.get(url, limit(deal)).on('error', function(e) {
-    console.error(e.message);
-});
+http.get(url, limit(deal));
